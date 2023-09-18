dd8ebf84e0688db54625ce47318031bba09ad935
Roman <110057821+Arkanad@users.noreply.github.com>
2023-09-18 21:25:31 +0300

    Add project files.

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<CityWeather> cities = new List<CityWeather>()
+        {
+            new CityWeather()
+            {
+                CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),
+                TemperatureFahrenheit = 33
+            },
+
+            new CityWeather()
+            {
+                CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),
+                TemperatureFahrenheit = 60
+            },
+
+            new CityWeather()
+            {
+                CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),
+                TemperatureFahrenheit = 82
+            }
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cities);
+        }
+
+        [Route("weather/{CityCode}")]
+        public IActionResult City(string? cityCode)
+        {
+            //if cityCode is not supplied in the route parameter
+            if (string.IsNullOrEmpty(cityCode))
+            {
+                //send null as model object to "Views/Weather/Index" view
+                return View();
+            }
+
+            //get matching city object based on the city code
+            CityWeather? city = cities.FirstOrDefault(temp => temp.CityUniqueCode == cityCode);
+
+            //send matching city object to "Views/Weather/Index" view
+            return View(city);
+        }
+    }
+}
